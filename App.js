c27f4aa0c186303ec763c8c0390a46de602d5904
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-09-08 12:42:27 +0700

    add: stylesheet

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <View
-      style={{
-        flex:1,
-        justifyContent: "center",
-        alignItems: "center",
-        
-      }}
+      style={myStyle.container}
     >
-      <Text style={{fontSize:20}}>Hello, this is Celesca.</Text>
-      <Text>Let's learn React Native together.</Text>
+      <Text style={myStyle.header}>Hello, this is Celesca.</Text>
+      <Text style={myStyle.content}>Let's learn React Native together.</Text>
     </View>
   );
 }
+
+const myStyle = StyleSheet.create({
+  container : {
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "orange",
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  conent: {
+    fontStyle: "italic"
+  }
+})
