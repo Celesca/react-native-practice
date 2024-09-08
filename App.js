e58c13209ff6173fd9cfc769721f617544fab4d1
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-09-08 13:19:50 +0700

    add: button and alerts

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, Button, Image, ScrollView, Alert } from "react-native";
 import { myStyle } from "./styles/myStyle";
 
 export default function App() {
   return (
-    <View
-      style={myStyle.container}
-    >
-      <Image source={require("./assets/icon.png")} style={myStyle.logo}></Image>
-      <Image source={{uri: "https://job.science.cmu.ac.th/upload/company/1-2022-12-15-225117972.png"}} style={myStyle.image}></Image>
-      <Text style={myStyle.header}>Hello, this is Celesca.</Text>
-      <Text style={myStyle.content}>Let's learn React Native together.</Text>
+    <View style={myStyle.container}>
+      <ScrollView>
+        <Image
+          source={require("./assets/icon.png")}
+          style={myStyle.logo}
+        ></Image>
+        <Image
+          source={{
+            uri: "https://job.science.cmu.ac.th/upload/company/1-2022-12-15-225117972.png",
+          }}
+          style={myStyle.image}
+        ></Image>
+        <Text style={myStyle.header}>Hello, this is Celesca.</Text>
+        <Text style={myStyle.content}>Let's learn React Native together.</Text>
+        <Button
+          title="ดูข้อมูลเพิ่มเติม"
+          color="red"
+          onPress={() =>
+            Alert.alert("แจ้งเตือน", "แอพนี้ถูกพัฒนาด้วย React Native")
+          }
+        ></Button>
+      </ScrollView>
     </View>
   );
 }
-
