13b768c37311ceba17b19df19b0b5e7161dc9740
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-09-08 12:56:17 +0700

    add: external stylesheet

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { View, Text, StyleSheet, Image } from "react-native";
-const logo = require("./assets/icon.png");
+import { myStyle } from "./styles/myStyle";
 
 export default function App() {
   return (
     <View
       style={myStyle.container}
     >
+      <Image source={require("./assets/icon.png")} style={myStyle.logo}></Image>
       <Image source={{uri: "https://job.science.cmu.ac.th/upload/company/1-2022-12-15-225117972.png"}} style={myStyle.image}></Image>
       <Text style={myStyle.header}>Hello, this is Celesca.</Text>
       <Text style={myStyle.content}>Let's learn React Native together.</Text>
@@ -13,23 +14,3 @@ export default function App() {
   );
 }
 
-const myStyle = StyleSheet.create({
-  container : {
-    flex:1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "orange",
-  },
-  header: {
-    fontSize: 30,
-    fontWeight: "bold",
-  },
-  conent: {
-    fontStyle: "italic"
-  },
-  image: {
-    height: 130,
-    width: 180,
-   marginBottom: 10 
-  }
-})
---
+++ styles/myStyle.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+export const myStyle = StyleSheet.create({
+    container : {
+      flex:1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "orange",
+    },
+    header: {
+      fontSize: 30,
+      fontWeight: "bold",
+    },
+    conent: {
+      fontStyle: "italic"
+    },
+    image: {
+      height: 130,
+      width: 180,
+     marginBottom: 10 
+    },
+    logo: {
+        height: 80,
+        width: 80,
+    }
+  })
