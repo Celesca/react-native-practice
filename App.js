733909d52edd7a07f5dd3728da240281b03ee738
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-09-08 12:51:34 +0700

    add: image

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+const logo = require("./assets/icon.png");
 
 export default function App() {
   return (
     <View
       style={myStyle.container}
     >
+      <Image source={{uri: "https://job.science.cmu.ac.th/upload/company/1-2022-12-15-225117972.png"}} style={myStyle.image}></Image>
       <Text style={myStyle.header}>Hello, this is Celesca.</Text>
       <Text style={myStyle.content}>Let's learn React Native together.</Text>
     </View>
@@ -24,5 +26,10 @@ const myStyle = StyleSheet.create({
   },
   conent: {
     fontStyle: "italic"
+  },
+  image: {
+    height: 130,
+    width: 180,
+   marginBottom: 10 
   }
 })
